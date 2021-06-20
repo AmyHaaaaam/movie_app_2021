48cd6aa34e6bd242dd454f37a69ced2705331d5b
함은영 <ham-eun-yeong@ham-eun-yeong-ui-MacBookAir.local>
2021-06-20 22:38:40 +0900

    https://github.com/AmyHaaaaam/movie_app_2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+//{fav} = props.fav
+function Food({ name, image }) {
+  return <div>
+    <img src={image} alt={name}/>
+    <h1>I like {name}</h1>
+  </div>
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:"https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg"
+  },
+  {
+    id: 2,
+    name: "ramen",
+    image: "https://www.kikkoman.eu/fileadmin/_processed_/0/0/csm_WEB_Traditional_Fukuoka_Ramen_0af581b084.jpg"
+  }
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} image={dish.image}/> )}
     </div>
-  );
+    );
 }
 
 export default App;
